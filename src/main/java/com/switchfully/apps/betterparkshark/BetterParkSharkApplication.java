78549f5e4e5d8a28b5855fa1e175d77febe6604e
nicolas bluj <nicolas_bluj@acslworld.com>
2025-05-07 15:09:40 +0200

    initial project setup <3

---
+++ src/main/java/com/switchfully/apps/betterparkshark/BetterParkSharkApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.apps.betterparkshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterParkSharkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterParkSharkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/apps/betterparkshark/BetterParkSharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.apps.betterparkshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterParkSharkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
