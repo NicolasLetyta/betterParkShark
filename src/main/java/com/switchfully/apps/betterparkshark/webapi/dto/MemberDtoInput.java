80073a3660f36c0b41aaecec01930136d597ddb4
nicolas bluj <nicolas_bluj@acslworld.com>
2025-05-08 15:48:06 +0200

    created Address, its dtos, its mapper, its repo created member, its dtos, its mappers created MembershipLevel

--- src/main/java/com/switchfully/apps/betterparkshark/webapi/dto/MemberDtoInput.java
+++ src/main/java/com/switchfully/apps/betterparkshark/webapi/dto/MemberDtoInput.java
@@ -1,4 +1,56 @@
 package com.switchfully.apps.betterparkshark.webapi.dto;
 
 public class MemberDtoInput {
+    private String firstName;
+    private String lastName;
+    private int phone;
+    private String email;
+    private String password;
+    private String licensePlate;
+    private AddressDtoInput addressDtoInput;
+    private int memberShipLevel;
+
+    public MemberDtoInput(String firstName, String lastName, int phone, String email, String password,
+                          String licensePlate, AddressDtoInput addressDtoInput, int memberShipLevel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.licensePlate = licensePlate;
+        this.addressDtoInput = addressDtoInput;
+        this.memberShipLevel = memberShipLevel;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public AddressDtoInput getAddressDtoInput() {
+        return addressDtoInput;
+    }
+
+    public int getMemberShipLevel() {
+        return memberShipLevel;
+    }
 }
---
+++ src/main/resources/db/migration/V3__init_rectification_price_hour_parking_lot_int_to_decimal.sql
@@ -0,0 +1,4 @@
+SET search_path TO parkshark;
+
+ALTER TABLE parking_lot
+ALTER COLUMN price_hour TYPE decimal
